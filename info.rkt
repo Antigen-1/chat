29a4f6311dbf1c429ca3ce6a4907b3f2118ab43e
ZhangHao <zhanghao@antigen.top>
2023-10-05 17:13:23 +0800

    Fix info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "chat")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "json"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/chat.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
